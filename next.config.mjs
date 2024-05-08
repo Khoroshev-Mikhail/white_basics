b350320c3cee4a9e3ffc04fd133699f8d25118eb
Mikha38 <oyks1379@yandex.ru>
2024-05-08 09:33:04 +0300

    Правки

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@
 const nextConfig = {
   transpilePackages: ['three'],
   reactStrictMode: true,
-  output: 'export',
+  // output: 'export',
   // basePath: '', если будут указаны пути, не забудьте проверить их в настройках вебака ниже
   // assetPrefix: '',
   images: {
