5011914dd72c482a2c9f7e43c1000e62b9d7d870
Mikha38 <oyks1379@yandex.ru>
2024-05-10 21:27:41 +0300

    Правки

--- components/05Partners.tsx
+++ components/05Partners.tsx
@@ -50,7 +50,7 @@ export default function Partners(){
         const button = document.getElementById('bottom_button')
         if(button){
             const buttonPosition = document.body.offsetHeight - button.getBoundingClientRect().y - button.offsetHeight - (window.innerHeight * 0.1)
-            setButtonPosition(buttonPosition)
+            setButtonPosition(buttonPosition < 300 ? buttonPosition : 155)
             setButtonHeight(button.offsetHeight)
         }
     }, [])
