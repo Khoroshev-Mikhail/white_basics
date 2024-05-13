052fefde197fde459472ccb1a1aff5a543adde9a
Mikha38 <oyks1379@yandex.ru>
2024-05-14 01:03:22 +0300

    Правки

--- components/04Tokenomics.tsx
+++ components/04Tokenomics.tsx
@@ -8,12 +8,12 @@ export default function Tokenomics(){
     return(
         <section className="relative z-30 bg-white _section  pt-[10px]">
             
-            <div data-aos="zoom-in" data-aos-duration="2000" className="absolute -z-10 top-0 left-[20px] 2xl:left-[calc((100%-1440px)/2)] w-[calc(100%-40px)] max-w-[1440px] h-full bg-[#F5F5F5] rounded-[10px]"></div>
+            <div data-aos="zoom-in" data-aos-duration="2000" className="absolute -z-10 top-0 left-[10px] 2xl:left-[calc((100%-1440px)/2)] w-[calc(100%-20px)] max-w-[1440px] h-full bg-[#F5F5F5] rounded-[10px]"></div>
             
             <div id="Tokenomics" className="_wrapper">
                 <div className="py-[30px] md:py-[60px]">
                     <h2 className="_h2 text-center">Tokenomics</h2>
-                    <div className=' px-2'>
+                    <div className='px-1'>
                         <Pie />
                     </div>
                     <div data-aos="zoom-in" data-aos-duration="2000" className='w-full overflow-hidden mt-[5%] px-5 text-center font-medium cursor-pointer '>
